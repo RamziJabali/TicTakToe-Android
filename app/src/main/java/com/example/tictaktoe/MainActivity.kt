958c03801fa377b08fa2de571e490781fa918fea
RamziJabali <ramzieljabali@gmail.com>
2021-06-11 18:43:43 -0700

    Added Game announcement

--- app/src/main/java/com/example/tictaktoe/MainActivity.kt
+++ app/src/main/java/com/example/tictaktoe/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tictaktoe
 
 import android.os.Bundle
 import android.widget.GridView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,11 +10,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var gridView: GridView
     private lateinit var viewModel: ViewModel
+    private lateinit var announcementTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        announcementTextView = findViewById(R.id.announcementTextView)
         gridView = findViewById(R.id.gridView)
         viewModel = ViewModel(this)
         viewModel.startGame()
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     fun setNewViewState(newViewState: ViewState, listener: ViewListener) {
         with(newViewState) {
             gridView.numColumns = boardSize
+            announcementTextView.text = gameAnnouncement
         }
     }
 
